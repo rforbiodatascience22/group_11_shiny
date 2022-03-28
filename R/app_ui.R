86d9d258b5d57660b95a08c9ded048257217561e
danielromeroy <s220260@student.dtu.dk>
2022-03-28 08:00:37 +0000

    task 3.2

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,7 +10,13 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      h1("centralDogma")
+      h1("centralDogma"),
+      tabsetPanel(
+        tabPanel(title = "panel1",
+                 "module1"),
+        tabPanel(title = "panel2",
+                 "module2"),
+      )
     )
   )
 }
